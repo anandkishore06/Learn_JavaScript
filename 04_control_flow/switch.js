e6d15b09150aeb062658cec904f87730334a28ca
Anand Kishore <101443071+anandkishore06@users.noreply.github.com>
2023-09-13 15:15:40 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+// switch case
+
+let month = 3
+
+switch (3) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case");
+        break;
+}
---
+++ 04_control_flow/ternaryOperator.js
@@ -0,0 +1,10 @@
+// Ternary Operator (condition?true:false)
+
+// find max number between two given Number
+
+let num1 = 20
+let num2 = 30
+
+let ans = num1 > num2 ? num1 : num2;
+
+console.log(ans);  // 30
