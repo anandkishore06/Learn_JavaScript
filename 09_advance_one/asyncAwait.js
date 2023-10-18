fc67f9bdc6662a3cd35e752355e5d1c83c46fe0c
Anand Kishore <101443071+anandkishore06@users.noreply.github.com>
2023-10-18 18:10:50 +0000

    await

--- 09_advance_one/asyncAwait.js
+++ 09_advance_one/asyncAwait.js
@@ -1,13 +1,51 @@
 /*
     * What is async ?
      -The async function declaration creates a binding of a new async function to a given name.
+    * What is await ??
+     -The await operator is used to wait for a Promise and get its fulfillment value. It can only be used inside an async function.
+
+     Note :  write await keyword infront of a promise
 
 */
 
-//Always returns a promise
+//async - Always returns a promise
 
-async function getData(){
-    return 'chai'
+const p  = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve('Promise Resolved Value!')
+    },3000)
+    
+})
+// async function getData(){
+//     return p;
+// }
+// const data = getData();
+// data.then((res)=>{
+//     console.log(res);
+// })
+  
+// console.log(data);
+// handle promise using async await
+
+async function handlePromise(){
+    console.log('Hello WOrld!'); // it will print quickly or firstly then at line no 33 ,it will wait for resolve for promise
+    // JS engine is waiting for promise to resolve 
+    const val = await p;
+    console.log(val);
+    console.log('JS');
+
+    const val2 = await p;
+    console.log(val2);
+    console.log('Java');
 }
-const data = getData();
-console.log(data);
+handlePromise();
+
+// handle promise using .then and .catch
+
+// function getData1(){
+//     p.then((res)=> console.log(res))
+//     console.log('Chai Or Code'); // JS ENgine will not wait for promise to resolved and print it first.
+// }
+// getData1()
+
+
